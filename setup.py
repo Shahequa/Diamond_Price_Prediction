c85b5ee38849ec6e5895bd19fd66543782e1785d
Shahequa <tsmodabbera017@gmail.com>
2023-10-29 15:38:39 +0530

    Model Trained

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -26,4 +25,5 @@ setup(
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
+
 
